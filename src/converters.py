c8c73334697e767ab1b15389de09c3552a9b5a75
samthecodeking <sam.leigh1987@gmail.com>
2025-02-13 16:39:36 +0000

    Wrote converter text_node_to_html_node

--- src/converters.py
+++ src/converters.py
@@ -1,4 +1,20 @@
-from textnode import TextType
+from textnode import TextType, TextNode
+from htmlnode import LeafNode
 
 def text_node_to_html_node(text_node):
-    pass
+    
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(text_node.text)
+        case TextType.BOLD:
+            return LeafNode( "b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", props={"alt": text_node.text, "src": text_node.url})
+        case TextType._:
+            raise Exception("not a valid text type")
